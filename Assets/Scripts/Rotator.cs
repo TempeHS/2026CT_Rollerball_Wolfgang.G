650b4ca8b36ae807811d3611eaa7e9be4730e136
Wolfgang G <wolfgang.gledhill@education.nsw.gov.au>
2026-05-01 10:40:24 +1000

    finishing up

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public float rotateSpeed = 36f;
+    public float speedVariance = 8f;
+
+    float actualRotateSpeed;
+
+    void Start()
+    {
+        actualRotateSpeed = rotateSpeed + Random.Range(-speedVariance, speedVariance);
+
+        // Random starting angle so spawned pickups do not line up visually.
+        transform.Rotate(0f, Random.Range(0f, 360f), 0f, Space.Self);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-        
+        transform.Rotate(new Vector3(0f, actualRotateSpeed, 0f) * Time.deltaTime);
     }
 }
